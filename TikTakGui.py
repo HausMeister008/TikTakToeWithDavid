c677e86f2875baa5f8a7e07498e5556ce1cc565d
León <leon.grass@gmx.net>
2022-01-08 22:08:24 +0100

    qt implementation
    Co-authored-by: daeaevaeaed <daeaevaeaed@users.noreply.github.com>

--- TikTakGui.py
+++ TikTakGui.py
@@ -1,3 +1,16 @@
-from PySide2.QtCore import *
-from PySide2.QtGui import *
-from PySide2.QtWidgets import *
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6 import QtSql
+from frm_main import Ui_MainWindow
+
+
+class Frm_main(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        
+
+app = QApplication()
+frm_main = Frm_main()
+frm_main.setStyleSheet(open('./stylesheet.qss', encoding="utf-8").read())
+frm_main.show()
+app.exec()
