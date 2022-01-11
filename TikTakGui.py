cf32b6c14ccf577d26e2119923f3eab493e571c1
daeaevaeaed <41583886+daeaevaeaed@users.noreply.github.com>
2022-01-11 17:17:08 +0100

    1st school edit
    gui grid

--- TikTakGui.py
+++ TikTakGui.py
@@ -7,10 +7,14 @@ class Frm_main(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        
+        # self.btn_ReloadQss.clicked.connect(load)
+      
+def load():
+    frm_main.setStyleSheet(open('./stylesheet.qss', encoding="utf-8").read())
+    
 
 app = QApplication()
 frm_main = Frm_main()
-frm_main.setStyleSheet(open('./stylesheet.qss', encoding="utf-8").read())
+load()
 frm_main.show()
 app.exec()
---
+++ ui_mainWindow.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'mainWindowHEVOKJ.ui'
+##
+## Created by: Qt User Interface Compiler version 6.2.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
+from PySide6.QtWidgets import (QApplication, QFrame, QLabel, QMainWindow,
+    QPushButton, QSizePolicy, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(1196, 891)
+        MainWindow.setStyleSheet(u"")
+        self.actionQuit = QAction(MainWindow)
+        self.actionQuit.setObjectName(u"actionQuit")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        font = QFont()
+        self.centralwidget.setFont(font)
+        self.lb_headline = QLabel(self.centralwidget)
+        self.lb_headline.setObjectName(u"lb_headline")
+        self.lb_headline.setGeometry(QRect(300, 0, 600, 70))
+        font1 = QFont()
+        font1.setPointSize(25)
+        font1.setBold(True)
+        font1.setItalic(False)
+        self.lb_headline.setFont(font1)
+        self.lb_headline.setCursor(QCursor(Qt.PointingHandCursor))
+        self.lb_headline.setAlignment(Qt.AlignCenter)
+        self.fr_player1 = QFrame(self.centralwidget)
+        self.fr_player1.setObjectName(u"fr_player1")
+        self.fr_player1.setGeometry(QRect(0, 0, 300, 900))
+        self.fr_player1.setFrameShape(QFrame.StyledPanel)
+        self.fr_player1.setFrameShadow(QFrame.Raised)
+        self.lb_player_1_headline = QLabel(self.fr_player1)
+        self.lb_player_1_headline.setObjectName(u"lb_player_1_headline")
+        self.lb_player_1_headline.setGeometry(QRect(10, 0, 280, 71))
+        self.lb_player_1_headline.setFont(font1)
+        self.lb_player_1_headline.setAlignment(Qt.AlignCenter)
+        self.lb_player_1_headline_2 = QLabel(self.fr_player1)
+        self.lb_player_1_headline_2.setObjectName(u"lb_player_1_headline_2")
+        self.lb_player_1_headline_2.setGeometry(QRect(10, 70, 280, 71))
+        font2 = QFont()
+        font2.setPointSize(18)
+        font2.setBold(True)
+        font2.setItalic(False)
+        self.lb_player_1_headline_2.setFont(font2)
+        self.lb_player_1_headline_2.setCursor(QCursor(Qt.PointingHandCursor))
+        self.lb_player_1_headline_2.setAlignment(Qt.AlignCenter)
+        self.lb_player_1_headline_3 = QLabel(self.fr_player1)
+        self.lb_player_1_headline_3.setObjectName(u"lb_player_1_headline_3")
+        self.lb_player_1_headline_3.setGeometry(QRect(10, 140, 280, 71))
+        self.lb_player_1_headline_3.setFont(font2)
+        self.lb_player_1_headline_3.setCursor(QCursor(Qt.PointingHandCursor))
+        self.lb_player_1_headline_3.setAlignment(Qt.AlignCenter)
+        self.lb_player_1_headline_4 = QLabel(self.fr_player1)
+        self.lb_player_1_headline_4.setObjectName(u"lb_player_1_headline_4")
+        self.lb_player_1_headline_4.setGeometry(QRect(10, 210, 280, 71))
+        self.lb_player_1_headline_4.setFont(font2)
+        self.lb_player_1_headline_4.setCursor(QCursor(Qt.PointingHandCursor))
+        self.lb_player_1_headline_4.setAlignment(Qt.AlignCenter)
+        self.fr_player2 = QFrame(self.centralwidget)
+        self.fr_player2.setObjectName(u"fr_player2")
+        self.fr_player2.setGeometry(QRect(900, 0, 300, 900))
+        font3 = QFont()
+        font3.setPointSize(18)
+        self.fr_player2.setFont(font3)
+        self.fr_player2.setFrameShape(QFrame.StyledPanel)
+        self.fr_player2.setFrameShadow(QFrame.Raised)
+        self.lb_player_2_headline = QLabel(self.fr_player2)
+        self.lb_player_2_headline.setObjectName(u"lb_player_2_headline")
+        self.lb_player_2_headline.setGeometry(QRect(10, 0, 280, 71))
+        self.lb_player_2_headline.setFont(font1)
+        self.lb_player_2_headline.setAlignment(Qt.AlignCenter)
+        self.lb_player_1_headline_7 = QLabel(self.fr_player2)
+        self.lb_player_1_headline_7.setObjectName(u"lb_player_1_headline_7")
+        self.lb_player_1_headline_7.setGeometry(QRect(10, 70, 280, 71))
+        self.lb_player_1_headline_7.setFont(font2)
+        self.lb_player_1_headline_7.setCursor(QCursor(Qt.PointingHandCursor))
+        self.lb_player_1_headline_7.setAlignment(Qt.AlignCenter)
+        self.lb_player_1_headline_6 = QLabel(self.fr_player2)
+        self.lb_player_1_headline_6.setObjectName(u"lb_player_1_headline_6")
+        self.lb_player_1_headline_6.setGeometry(QRect(10, 210, 280, 71))
+        self.lb_player_1_headline_6.setFont(font2)
+        self.lb_player_1_headline_6.setCursor(QCursor(Qt.PointingHandCursor))
+        self.lb_player_1_headline_6.setAlignment(Qt.AlignCenter)
+        self.lb_player_1_headline_5 = QLabel(self.fr_player2)
+        self.lb_player_1_headline_5.setObjectName(u"lb_player_1_headline_5")
+        self.lb_player_1_headline_5.setGeometry(QRect(10, 140, 280, 71))
+        self.lb_player_1_headline_5.setFont(font2)
+        self.lb_player_1_headline_5.setCursor(QCursor(Qt.PointingHandCursor))
+        self.lb_player_1_headline_5.setAlignment(Qt.AlignCenter)
+        self.btn_ReloadQss = QPushButton(self.fr_player2)
+        self.btn_ReloadQss.setObjectName(u"btn_ReloadQss")
+        self.btn_ReloadQss.setGeometry(QRect(150, 830, 130, 50))
+        font4 = QFont()
+        font4.setFamilies([u"Calibri"])
+        font4.setPointSize(18)
+        font4.setBold(True)
+        font4.setItalic(False)
+        font4.setUnderline(False)
+        font4.setStrikeOut(False)
+        self.btn_ReloadQss.setFont(font4)
+        self.fr_Gameboard = QFrame(self.centralwidget)
+        self.fr_Gameboard.setObjectName(u"fr_Gameboard")
+        self.fr_Gameboard.setGeometry(QRect(300, 70, 600, 830))
+        self.fr_Gameboard.setFrameShape(QFrame.StyledPanel)
+        self.fr_Gameboard.setFrameShadow(QFrame.Raised)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.actionQuit.setText(QCoreApplication.translate("MainWindow", u"Quit", None))
+        self.lb_headline.setText(QCoreApplication.translate("MainWindow", u"TicTacToe", None))
+        self.lb_player_1_headline.setText(QCoreApplication.translate("MainWindow", u"Player1", None))
+        self.lb_player_1_headline_2.setText(QCoreApplication.translate("MainWindow", u"Name: ", None))
+        self.lb_player_1_headline_3.setText(QCoreApplication.translate("MainWindow", u"Guthaben: ", None))
+        self.lb_player_1_headline_4.setText(QCoreApplication.translate("MainWindow", u"Einsatz: ", None))
+        self.lb_player_2_headline.setText(QCoreApplication.translate("MainWindow", u"Player2", None))
+        self.lb_player_1_headline_7.setText(QCoreApplication.translate("MainWindow", u"Name: ", None))
+        self.lb_player_1_headline_6.setText(QCoreApplication.translate("MainWindow", u"Einsatz: ", None))
+        self.lb_player_1_headline_5.setText(QCoreApplication.translate("MainWindow", u"Guthaben: ", None))
+        self.btn_ReloadQss.setText(QCoreApplication.translate("MainWindow", u"ReloadQss", None))
+    # retranslateUi
+
